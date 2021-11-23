30ab7cf51db837abd1ee74ef368b17642b47caa8
Shania Tsamara <shaniatsamara@gmail.com>
2021-11-23 13:46:10 +0800

    implemented the CRUD method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,12 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def require_task_params
